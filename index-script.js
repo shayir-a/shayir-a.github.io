1bfc03992f46258ac9a88acb9ece91def68652ae
shayir456 <sa03566@uga.edu>
2020-05-14 20:02:38 -0400

    Worked on Cover and About

---
+++ index-script.js
@@ -0,0 +1,12 @@
+function openSidebar() {
+    document.getElementById("main").style.marginLeft = "25%";
+    document.getElementById("sidebar").style.width = "25%";
+    document.getElementById("sidebar").style.display = "block";
+    document.getElementById("openSidebar").style.display = "none"; 
+}
+
+function closeSidebar() {
+    document.getElementById("main").style.marginLeft = "0%";
+    document.getElementById("sidebar").style.display = "none";
+    document.getElementById("openSidebar").style.display = "inline-block"; 
+}
